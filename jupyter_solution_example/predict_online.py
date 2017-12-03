98868f202ce6d863df769115de34b6fab02917fe
ale3otik <aleksey.zotov@phystech.edu>
2017-12-03 05:07:41 +0300

    predict online

--- jupyter_solution_example/predict_online.py
+++ jupyter_solution_example/predict_online.py
@@ -36,22 +36,28 @@ class MyClient(hackathon_protocol.Client):
     def on_header(self, csv_header):
         print('onheader')
         self.header = {column_name: n for n, column_name in enumerate(csv_header)}
+        self.columns = csv_header
+        print(csv_header)
         # print("Header:", self.header)
 
     def on_orderbook(self, cvs_line_values):
         if cvs_line_values[0] == 'COFFEE':
             return
+
         print('onorderbook')
         
 
         self.buffer_main = self.buffer_main.append(
-            pd.DataFrame(np.array([np.array(cvs_line_values)]), columns=list(self.header.keys())), ignore_index=True)
+            pd.DataFrame(np.array([np.array(cvs_line_values)]), columns=self.columns), ignore_index=True)
         print(self.buffer_with_features.shape)
+        print(self.buffer_main.shape)
 
-        if self.buffer_main.shape[0] > self.win_size:
+        if self.buffer_main.shape[0] >= self.win_size:
+            print(self.buffer_main.columns)
+            print(self.buffer_main.iloc[0])
             features = count_all_features(self.buffer_main.iloc[-self.win_size:])
             to_append = pd.DataFrame(np.concatenate([np.array(cvs_line_values), features]))
-            print(to_append.shape)
+            print('to_append', to_append.shape)
 
             self.buffer_with_features = self.buffer_with_features.append(to_append, ignore_index=True)
             if self.buffer_with_features.shape[0] > self.win_size:
